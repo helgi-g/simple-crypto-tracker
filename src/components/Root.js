16d10ddb3259f2cf9452a04b9a1cd51c567dac06
Helgi-G <helgi.mail.box@gmail.com>
2018-02-08 14:21:52 +0300

    initial project version

---
+++ src/components/Root.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './style.css'
+class Root extends Component {
+
+  render() {
+    return (
+      <div className='red'>
+        App!!!
+      </div>
+    )
+  }
+}
+
+export default Root
---
+++ webpack.prod.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: "source-map",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'bundle.js',
+    publicPath: '/js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })]
+};
