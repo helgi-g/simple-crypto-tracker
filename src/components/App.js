c67a464d9dea5edab2fce29bf6aa9d5f473c964e
Helgi-G <helgi.mail.box@gmail.com>
2018-02-11 21:55:09 +0300

    add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { plus } from '../AC/index'
 import './style.css'
 
@@ -9,9 +10,13 @@ class App extends Component {
     return (
     <div>
         <button onClick={this.props.plus} className='red'>{this.props.counter}</button>
+        <Switch>
+          <Route path='/error' render={this.getError} />
+        </Switch>
     </div>  
     )
   }
+  getError = () => <h1>Some Error</h1>
 }
 export default connect((state) => {
   return {
